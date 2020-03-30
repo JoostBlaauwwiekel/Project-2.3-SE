69c4598d907a939410ec85197f4245b083ec9af6
Legerdy <legerdyl1@gmail.com>
2020-03-30 15:38:22 +0200

    Reversi game logic
    Game logic temporarily added in ReversiBoard, eventually will be moved to a different class.

--- GameFramework/GameBoardTest.java
+++ GameFramework/GameBoardTest.java
@@ -5,12 +5,43 @@ import TicTacToeGame.TicTacToeBoard;
 
 public class GameBoardTest {
     public static void main(String[] args){
-        System.out.println("Tac tac toe board:");
+        // Make tic tac toe board and print it
+        System.out.println("Tic tac toe board:");
         TicTacToeBoard tictactoeBoard = new TicTacToeBoard();
         tictactoeBoard.printBoard();
 
+        // Make reversi board and print it
         System.out.println("Reversi board:");
         ReversiBoard reversiBoard = new ReversiBoard();
+        reversiBoard.printBoard();
+
+        // Print reversiboard disc counts
+        System.out.println("Currently there are " + reversiBoard.getDiscCount(0) + " unoccupied positions, "
+                + reversiBoard.getDiscCount(1) + " white discs and "
+                + reversiBoard.getDiscCount(2) + " black discs.");
+
+        // Print valid moves default board
+        System.out.print("Current valid moves for white are: ");
+        for(int move : reversiBoard.getMoves(2)){
+            System.out.print(move + " ");
+            reversiBoard.setBoardPos(move, 3);
+        }
+        System.out.println(".");
+
+        System.out.print("Current valid moves for black are: ");
+        for(int move : reversiBoard.getMoves(1)){
+            System.out.print(move + " ");
+            reversiBoard.setBoardPos(move, 4);
+        }
+        System.out.println(".");
+
+        reversiBoard.printBoard();
+
+        // Print valid moves for test case for black player
+        reversiBoard.setTestBoard2();
+        for(int move : reversiBoard.getMoves(2)){
+            reversiBoard.setBoardPos(move, 3);
+        }
         reversiBoard.printBoard();
     }
 }
